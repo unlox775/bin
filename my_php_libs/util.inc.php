de95f414f7c389e443166bc5ad5b1cc78401418c
Dave Buchanan <dave@joesvolcano.net>
2015-04-13 13:35:26 -0600

    more new CLI utils

--- my_php_libs/util.inc.php
+++ my_php_libs/util.inc.php
@@ -2,3 +2,6 @@
 
 ///  Useful util
 function db_quote($str) { return is_null($str) ? 'NULL' : (is_numeric($str) ? $str : "'". mysql_real_escape_string( $str ) ."'"); }
+
+function coalesce() { foreach( func_get_args() as $v ) if (! is_null($v) ) return $v; }
+function _or()      { foreach( func_get_args() as $v ) if (!   empty($v) ) return $v; }
